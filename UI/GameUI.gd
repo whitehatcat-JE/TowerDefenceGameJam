672ba7b9a09865b21c46baa7ef71bb2bcd5c2b06
poshpotato <56280387+poshpotato@users.noreply.github.com>
2023-07-22 13:30:01 +1200

    Added labels and update methods to Health and XP bars. Restored GUI.tscn.

---
+++ UI/GameUI.gd
@@ -0,0 +1,23 @@
+extends Control
+
+func _ready():
+	pass
+
+func update_health_bar(currentHealth:float, maxHealth:float):
+	$HealthBar.set_max(maxHealth)
+	$HealthBar.set_value(currentHealth)
+	$HealthBar/Label.set_text(String.num(currentHealth,0)+"/"+String.num(maxHealth,0))
+
+func update_xp_bar(currentXP:float, maxXP:float):
+	$XPBar.set_max(maxXP)
+	$XPBar.set_value(currentXP)
+	$XPBar/Label.set_text(String.num(currentXP,0)+"/"+String.num(maxXP,0))
+	
+func debug_rand_change():
+	var mH = randf_range(1,500)
+	var cH = randf_range(0,mH)
+	var mX = randf_range(1,500)
+	var cX = randf_range(0,mX)
+	update_health_bar(cH,mH)
+	update_xp_bar(cX,mX)
+	
