430d387e711f4d27bf407dbe5d59564b16a630e5
Jayden <everesja@gmail.com>
2023-07-22 17:39:02 +1200

    Added knockback

--- main/objects/enemy.gd
+++ main/objects/enemy.gd
@@ -7,7 +7,7 @@ var speed:float = 50.0
 var explodeRange:float = 10.0
 var target:Vector2
 var rectMax = 50
-@onready var hpBar = $ColorRect2 
+@onready var hpBar = $healthbarPivot/overHealth
 
 @onready var pivot:Node = $enemyPivot
 @onready var director:Node = $enemyPivot/enemyDirector
@@ -16,13 +16,14 @@ func die():
 	queue_free()
 
 func _physics_process(delta):
-	pivot.look_at(target)
-	pivot.rotation_degrees += 90
+	look_at(target)
+	rotation_degrees += 90
+	$healthbarPivot.global_rotation = 0
 	velocity = (director.global_position - position) * speed
 	move_and_slide()
 
 func damage(amt:float):
 	currentHealth -= amt
 	hpBar.set_size(Vector2((currentHealth/healthMax) * rectMax, 5))
-	if currentHealth <= 0.0:
-		die()
+	if currentHealth <= 0.0: die();
+	else: $knockbackAnim.play("knockback");
--- player/Player.gd
+++ player/Player.gd
@@ -1,18 +1,11 @@
 extends CharacterBody2D
 
-#Hurt effect when done
-#const playerhurt = preload("res://")
-#States
-enum {
-	MOVE,
-	ATTACK
-}
 #All The Variables needed throughout the code
 var acceleration = 1000
 var maxSpeed = 220
 var friction = 1700
 
-var state = MOVE
+var attacking:bool = false
 
 #onready var ap = $AnimationPlayer
 #onready var at = $AnimationTree
@@ -22,14 +15,6 @@ var state = MOVE
 
 #runs every frame to load which ever state has been most recently called
 func _process(delta):
-	match state:
-		MOVE:
-			move_state(delta)
-		ATTACK:
-			attack_state(delta)
-
-#Move state has all move functions
-func move_state(delta):
 	var ip = Vector2.ZERO
 #	(Left - Right) + (up - Down) Equals direction of travel,
 # Nomalized stops the diagonal from being longer than normal
@@ -38,30 +23,23 @@ func move_state(delta):
 	ip = ip.normalized()
 	
 	if ip != Vector2.ZERO:
-		#sh.kbv = ip
-#		loading animations
-		#at.set("parameters/idle/blend_position", ip)
-		#at.set("parameters/Run/blend_position", ip)
-		#at.set("parameters/Attack/blend_position", ip)
-		#ans.travel("Run")
 		velocity = velocity.move_toward(ip * maxSpeed, acceleration * delta)
 	else: 
 		#ans.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 	move()
 	
-	if Input.is_action_just_pressed("attack"):
-		print("attack")
-		state = ATTACK
+	if Input.is_action_just_pressed("attack") and !attacking:
+		attack()
 
-#simple attack state
- #warning-ignore:unused_argument
-func attack_state(delta):
-	velocity = Vector2.ZERO
+func attack():
+	attacking = true
+	$playerSprite.play("attack")
 	$SwordHitbox/Hitbox.set_deferred("monitoring", true)
-	await get_tree().create_timer(0.25).timeout
-	#ans.travel("Attack")
-	state = MOVE
+	await get_tree().create_timer(0.5).timeout
+	$SwordHitbox/Hitbox.set_deferred("monitoring", false)
+	$playerSprite.play("idle")
+	attacking = false
 
 func move():
 	move_and_slide()
